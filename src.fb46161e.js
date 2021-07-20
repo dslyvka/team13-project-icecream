88c53014163754a2cfbfa8985d94343f27984793
dslyvka <70968697+dslyvka@users.noreply.github.com>
2021-07-20 20:04:07 +0000

    Deploying to gh-pages from @ dslyvka/team13-project-icecream@f2961f0b358d9d1131fa7f63fde89423d63f6497 🚀

--- src.fb46161e.js
+++ src.fb46161e.js
@@ -5,4 +5,4 @@ const e=document.querySelectorAll('a[href^="#"]');for(let t of e)t.addEventListe
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/scroll");
 },{"./sass/main.scss":"clu1","./js/scroll":"N8YO"}]},{},["Focm"], null)
-//# sourceMappingURL=/team13-project-icecream/src.0c33384d.js.map
+//# sourceMappingURL=/team13-project-icecream/src.fb46161e.js.map
