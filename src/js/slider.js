7a2f60806d0d60a0e1294bfd3cc5c20bee1edb3e
dslyvka <daniilslyvka@gmail.com>
2021-07-21 19:20:06 +0300

    slider and fixes

---
+++ src/js/slider.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.slider').slick({
+      arrows: false,
+      dots: true,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      slidesToScroll: 1,
+    });
+});
